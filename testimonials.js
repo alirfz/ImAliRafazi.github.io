071f42693ff430df91d756c18c162fe40750d09d
Seyedali Rafazi <63471803+alirfz@users.noreply.github.com>
2022-10-31 10:29:24 -0700

    Add files via upload

---
+++ testimonials.js
@@ -0,0 +1,26 @@
+const testimonials = [
+    {
+        quote: "“ You can teach a student a lesson for a day; but if you can teach him to learn by creating curiosity, he will continue the learning process as long as he lives “",
+        avatar: "avatar1",
+        name: "Diana Ayi",
+        title: "Musician",
+    },
+    {
+        quote: "“ Real education should consist of drawing the goodness and the best out of our own students. What better books can there be than the book of humanity “",
+        avatar: "avatar2",
+        name: "Ernest Achiever",
+        title: "Web Developer",
+    },
+    {
+        quote: "“My philosophy of education is that all children are unique and must have a stimulating educational environment  “",
+        avatar: "avatar3",
+        name: "Asamoah Gyan",
+        title: "Footerballer",
+    },
+    {
+        quote: "“WE ARE WHAT WE REPEATEDLY DO. EXCELLENCE, THEN, IS NOT AN ACT BUT A HABIT“",
+        avatar: "avatar4",
+        name: "Beatrica Aku",
+        title: "Politician",
+    },
+]
